a975936c5ec7fc83cb9be1ac3015dcd088bccf69
yudataguy <yudataguy@users.noreply.github.com>
2024-01-03 13:38:52 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="yudataguy"
+original_name="pip_test"
+original_urlname="pip-test"
+original_description="Awesome pip_test created by yudataguy"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pip_test $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- pip_test/cli.py
+++ pip_test/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for pip_test project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m pip_test` and `$ pip_test `.
 
     This is your program's entry point.
 
